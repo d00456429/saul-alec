b8b01dcc2246ee3b4ec0fb049ec5809feb215e3b
Saul Aguiar <d00456429@CMP-SCC112-1107.local>
2024-06-07 12:42:25 -0600

    improvements

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ let flip = document.getElementById("flip");
 let result = document.getElementById("result");
 
 let choice = () => {
-	let choice = Math.floor(Math.random() * 2);
-	return choice;
+  let choice = Math.floor(Math.random() * 2);
+  return choice;
 };
 
 flip.onclick = () => {
-	let x = choice();
-	if (x === 0) {
-		result.innerHTML = "Heads";
-	} else {
-		result.innerHTML = "Tails";
-	}
+  let x = choice();
+  if (x === 0) {
+    result.innerHTML = "Heads";
+    document.getElementById("result").style.color = "blue";
+  } else {
+    result.innerHTML = "Tails";
+    document.getElementById("result").style.color = "red";
+  }
 };
